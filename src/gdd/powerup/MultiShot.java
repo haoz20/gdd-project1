ee6ca6d788742cdc0622ea24510361a4fac1be6a
haoz20 <zhaonianhao2005@gmail.com>
2025-07-18 18:38:39 +0700

    add powerup multishot in upgrade method

--- src/gdd/powerup/MultiShot.java
+++ src/gdd/powerup/MultiShot.java
@@ -2,6 +2,10 @@ package gdd.powerup;
 
 import gdd.sprite.Player;
 
+import javax.swing.*;
+
+import static gdd.Global.*;
+
 public class MultiShot extends PowerUp {
 
     public MultiShot(int x, int y) {
@@ -10,7 +14,11 @@ public class MultiShot extends PowerUp {
 
     @Override
     public void upgrade(Player player) {
-
+        ImageIcon ii = new ImageIcon(IMG_POWERUP_MULTISHOT);
+        var scaledImage = ii.getImage().getScaledInstance(ii.getIconWidth() ,
+                ii.getIconHeight() ,
+                java.awt.Image.SCALE_SMOOTH);
+        setImage(scaledImage);
     }
 
 
